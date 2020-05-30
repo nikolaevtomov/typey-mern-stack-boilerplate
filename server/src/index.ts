22d4130be4e95014337077221b90c4461cbb084f
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-05-30 18:03:51 +0100

    Move dev dependency to main package file so there will be no ducpications

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 
@@ -12,10 +12,6 @@ app.use(express.static(path.resolve(process.cwd(), '..', 'client', 'dist')));
 app.use(bodyParser.json());
 
 app.use('/api', router);
-
-app.use((_req: Request, res: Response) => {
-  res.status(404).send({ error: 'Page not found' });
-});
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
--- server/src/router.ts
+++ server/src/router.ts
@@ -12,4 +12,8 @@ router.post('/message', (req: Request, res: Response) => {
   res.send(req.body);
 });
 
+router.use((_req: Request, res: Response) => {
+  res.status(404).send({ error: 'Page not found' });
+});
+
 export default router;
