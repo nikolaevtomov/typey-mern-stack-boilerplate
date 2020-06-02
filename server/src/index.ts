277239eae1d0514b1970ffb965b271c176e0b2e3
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-06-02 10:14:17 +0100

    Serve html file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,23 @@
 import express from 'express';
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 import path from 'path';
 
-// import router from '^/router';
+import router from '^/router';
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.use(express.static(path.resolve(process.cwd(), '..', 'public')));
-// app.use(express.static('public'));
-// app.use(express.static(path.resolve(process.cwd(), '..', 'client', 'dist')));
 
-// app.use('/api', router);
+app.get('/', (_req, res) => {
+  res.sendFile('index.html', {
+    root: path.resolve(process.cwd(), '..', 'public'),
+  });
+});
+
+app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
