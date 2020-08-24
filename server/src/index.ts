e9a6b3256ec46f6231000c1e2868da7bd9dd4af3
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-08-24 08:26:10 +0100

    Test router revert

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,13 +27,13 @@ app.use(express.static(path.resolve(process.cwd(), 'client', 'dist')));
 // NOTE: Works for local but not for production
 // app.use(express.static(path.resolve(process.cwd(), '..', 'client', 'dist')));
 
+app.use('/api', router);
+
 app.get('/*', (_req, res) => {
   res.sendFile('index.html', {
     root: path.join(path.resolve(process.cwd(), '..', 'client', 'dist')),
   });
 });
-
-app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
